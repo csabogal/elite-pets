31112e2accde8293aee38bde55e6dbf8c5cea0a2
Camilo Sabogal <camilo.sabogal@hotmail.com>
2025-04-29 13:01:04 -0500

    implementación de Angular: estructuras de carpetas y componentes, servicios y navegación entre las paginas

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PetsComponent } from './pages/pets/pets.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'pets', component: PetsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' }, // Redirige rutas no encontradas al home
+];
