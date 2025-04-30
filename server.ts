15cb92611814bad6d6c85493fee8d4a732d3c469
Camilo Sabogal <camilo.sabogal@hotmail.com>
2025-04-30 00:26:59 -0500

    nuevos cambios para el despliegue

--- server.ts
+++ server.ts
@@ -8,6 +8,13 @@ export async function netlifyAppEngineHandler(
 ): Promise<Response> {
   const context = getContext();
 
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
   const result = await angularAppEngine.handle(request, context);
   return result || new Response('Not found', { status: 404 });
 }
